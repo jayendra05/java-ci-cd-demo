47e3cfb949cf7e2a378fc53ae859cc161ea591c3
Jayendra <jayendra06mishra@gmail.com>
2026-05-04 23:59:19 +0530

    adding new line

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello CI/CD World!");
+        System.out.println("This is a simple Java application to demonstrate CI/CD pipeline.");
     }
 }
